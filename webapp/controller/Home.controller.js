08b3b97573b1dceb7263b38c0c671b09458f59a9
Evangelos CHRISTODOULIDIS <echristodoulidis@deloitte.gr>
2022-10-05 15:01:41 +0000

    End of Day 1

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,19 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast) {
+    "use strict";
 
-        return Controller.extend("btp.academy.fiorifirstapp.controller.Home", {
-            onInit: function () {
+    return Controller.extend("btp.academy.fiorifirstapp.controller.Home", {
+      onInit: function () {},
 
-            }
-        });
+      onPress: function (evt) {
+        // MessageToast.show(evt.getSource().getId() + " Toasty!!!");
+
+        MessageToast.show(evt.getParameter("id") + " Toasty!!!");
+      },
     });
+  }
+);
